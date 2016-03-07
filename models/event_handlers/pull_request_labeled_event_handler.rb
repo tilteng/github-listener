e62bf02591121204f0ad062e3629dbfb54a05ed5
cohitre <carlosrr@gmail.com>
2016-03-07 15:43:22 -0800

    downcasing label for easier matching

--- models/event_handlers/pull_request_labeled_event_handler.rb
+++ models/event_handlers/pull_request_labeled_event_handler.rb
@@ -2,8 +2,7 @@ require_relative './base_event_handler'
 
 class PullRequestLabeledEventHandler < BaseEventHandler
   def execute!(slack, slack_room_id)
-    puts label
-    if label == 'Needs review'
+    if label.downcase == 'needs review'
       slack.post_message(slack_room_id, "[#{repository_link} #{pull_request_link}] #{target_user_login}: Needs review :git:>>>#{title}")
       self.random!(slack, slack_room_id)
     end
